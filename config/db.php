5891bbf31c0e88242b6d694e9401daae434f1911
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-12-15 14:09:25 +0300

    Getting db configs from local {root}/.env file

--- config/db.php
+++ config/db.php
@@ -1,9 +1,15 @@
 <?php
 
+use M1\Env\Parser;
+
+// \Yii::getAlias('@webroot') <---- if need root path
+$env = new Parser(file_get_contents('../.env'));
+$env_conf = $env->getContent();
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host='.$env_conf['DB_HOST'].';dbname='.$env_conf['DB_NAME'],
+    'username' => 'root' || $env_conf['DB_USER'],
+    'password' => '' || $env_conf['DB_PASS'],
     'charset' => 'utf8',
 ];
