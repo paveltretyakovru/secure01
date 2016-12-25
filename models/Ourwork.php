388fc55d09b45cc638b9bdc39c2ca1b1e2a02dcf
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-12-26 01:51:12 +0300

    Created Ourwork model

---
+++ models/Ourwork.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ourworks".
+ *
+ * @property integer $id
+ * @property string $url
+ * @property string $title
+ */
+class Ourwork extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ourworks';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['url', 'title'], 'required'],
+            [['url', 'title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'url' => 'Url',
+            'title' => 'Title',
+        ];
+    }
+}
